f4bc707045f0fbca6bab919081d4a5b7e9bf2dcb
SilverLongjohns <jakehollinger@hotmail.co.uk>
2020-09-08 15:57:48 +0100

    mocked dependencies in Account tests

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,10 +1,16 @@
 require "account.rb"
 
 describe Account do
-  subject { described_class.new(Balance.new) }
+  let(:balance) {double :balance}
+
+  subject { described_class.new(balance) }
+
 
   it "should be able to print a statement" do
-    subject.deposit(50.00)
+    allow(balance).to receive(:dates).and_return(["08/09/2020"])
+    allow(balance).to receive(:statement).and_return([{:deposit => 50}])
+    allow(balance).to receive(:total).and_return([50])
+    allow(balance).to receive(:total_balance).and_return([50])
     expect(subject.print_statement).to eq(
     "date || credit || debit || balance
 08/09/2020 || 50.00 ||  || 50.00"
@@ -12,8 +18,10 @@ describe Account do
   end
 
   it "should print a statement with withdrawals and deposits" do
-    subject.deposit(50)
-    subject.withdraw(10)
+    allow(balance).to receive(:dates).and_return(["08/09/2020", "08/09/2020"])
+    allow(balance).to receive(:statement).and_return([{:deposit => 50}, {:withdrawal => 10}])
+    allow(balance).to receive(:total).and_return([40])
+    allow(balance).to receive(:total_balance).and_return([50, 40])
     expect(subject.print_statement).to eq(
     "date || credit || debit || balance
 08/09/2020 || 50.00 ||  || 50.00
